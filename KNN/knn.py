97747e0e914f7ba90faa83f18fc80bdde18eb18f
Lang-Shin <bajuracharles@gmail.com>
2026-04-27 02:50:22 +0800

    Added a function

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,5 +1,33 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+def custom_train_test(features, labels, train_size=0.8):
+    n = len(features)
+
+    indices = np.arange(n)
+
+    np.random.shuffle(indices)
+
+    split_point = (indices*train_size)
+
+    train_data_id = indices[:int(split_point)]
+    test_data_id = indices[int(split_point):]
+
+    x_train, x_test = features[train_data_id], features[test_data_id]
+    y_train, y_test = labels[train_data_id], labels[test_data_id]
+
+    return x_train, x_test, y_train, y_test
+
 
 df = pd.read_csv("customer_segmentation_data.csv")
 
+data_arr = df.values
+
+features = data_arr[:, :-1]
+labels = data_arr[:,-1]
+
 print(df)
+
+print(features)
+print(labels)

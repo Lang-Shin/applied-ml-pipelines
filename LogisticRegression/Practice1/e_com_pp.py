7cdc2998cd5dd5a18b341a295dd7c412a21b286c
Charles <bajuracharles@gmail.com>
2026-06-27 21:35:48 +0800

    Feat : Added data for Practice1

---
+++ LogisticRegression/Practice1/e_com_pp.py
@@ -0,0 +1,10 @@
+"""
+    E-Commerce Purchase Prediction
+    Feat : 
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+

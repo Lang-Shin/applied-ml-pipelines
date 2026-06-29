319e476d4f83668e335fd146bce2caebf7a9b525
Charles <bajuracharles@gmail.com>
2026-06-29 16:57:44 +0800

    Practice1 --> Done

--- KNN/Practice1/promotion_pred.py
+++ KNN/Practice1/promotion_pred.py
@@ -35,14 +35,14 @@ param_grid = {
     'classifier__n_neighbors' : [3, 5, 7, 9]
 }
 
-grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='accuracy')
+grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
 grid_search.fit(X_train, y_train)
 
 predictions = grid_search.predict(X_test)
 
-print("\n\n", classification_report(y_test, predictions), "\n\n")
+print("\n\n", classification_report(y_test, predictions))
 
-for pred, act in zip(predictions[:20], y_test[:20]):
+for pred, act in zip(predictions[:20], y_test.iloc[:20]):
     print(f"Prediction : {pred}  -- Actual : {act}")
     
 

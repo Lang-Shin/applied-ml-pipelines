b2f7d00d72d035cd17bd34a196e46cc585b8552e
Charles <bajuracharles@gmail.com>
2026-06-29 14:12:33 +0800

    Practice3 --> DONE

--- LogisticRegression/Practice3/ps_prediction.py
+++ LogisticRegression/Practice3/ps_prediction.py
@@ -3,13 +3,14 @@
     Feat : Age, Monthly_Hours_Log, Customer_Support_Calls, Device_Type, Discount_Offered, Satisfaction_Score, Premium_User
 """
 
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.metrics import log_loss, confusion_matrix, accuracy_score, RocCurveDisplay, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 
 df = pd.read_csv("LogisticRegression/Practice3/user_desc.csv")
@@ -37,5 +38,45 @@ preprocessor = ColumnTransformer(
 
 model = Pipeline([
     ("transformers", preprocessor),
-    ("classifier", LogisticRegression)
+    ("classifier", LogisticRegression())
 ])
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+proba = model.predict_proba(X_test)[:,1]
+loss = log_loss(y_test, proba)
+
+classifier = model.named_steps['classifier']
+
+accuracy = accuracy_score(y_test, predictions)
+cm = confusion_matrix(y_test, predictions)
+print(f"\n{cm}")
+print(f"\nAccuracy : {(accuracy*100):.2f}%")
+print(f"\nLoss : {loss:.2f}")
+print(f"\nBias : {classifier.intercept_[0]}")
+
+feat_names = model.named_steps['transformers'].get_feature_names_out()
+
+for feat, weight in zip(feat_names, classifier.coef_[0]):
+    print(f"{feat:50} -- {weight}")
+
+
+fig, axes = plt.subplots(1,3, figsize=(12,5))
+ConfusionMatrixDisplay.from_predictions(y_test, predictions, ax=axes[0])
+axes[0].set_title("Confusion Matrix")
+
+axes[1].bar(feat_names, classifier.coef_[0], color="#000000")
+axes[1].grid(True, alpha=0.6)
+axes[1].set_xlabel("Features")
+axes[1].set_ylabel("Weights")
+axes[1].set_title("Feature Coefficient")
+axes[1].tick_params(axis='x', rotation=60)
+
+RocCurveDisplay.from_estimator(model, X_test, y_test, ax=axes[2])
+axes[2].grid(True, alpha=0.6)
+axes[2].set_title("Roc Curve")
+
+plt.tight_layout()
+plt.show()

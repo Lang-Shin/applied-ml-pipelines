6cbec9fb8087f137748368018739e996d5152465
Charles <bajuracharles@gmail.com>
2026-06-30 14:31:45 +0800

    Practice1 --> Done

--- KNN/Practice1/promotion_pred.py
+++ KNN/Practice1/promotion_pred.py
@@ -4,7 +4,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import classification_report, ConfusionMatrixDisplay
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay, accuracy_score
 import matplotlib.pyplot as plt
 
 
@@ -38,16 +38,38 @@ param_grid = {
 grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
 grid_search.fit(X_train, y_train)
 
+cv_results = pd.DataFrame(grid_search.cv_results_)
+df_result = cv_results[['param_classifier__n_neighbors', 'mean_test_score', 'std_test_score']]
+df_result = df_result.sort_values(by='param_classifier__n_neighbors').reset_index(drop=True)
+
 predictions = grid_search.predict(X_test)
 
+best_k = df_result.iloc[df_result['mean_test_score'].idxmax()]
+worst_k = df_result.iloc[df_result['mean_test_score'].idxmin()]
+
+print("\n\n",df_result)
+print(f"\nBest K :\n{best_k}")
+print(f"\nWorst K:\n{worst_k}")
 print("\n\n", classification_report(y_test, predictions))
 
 for pred, act in zip(predictions[:20], y_test.iloc[:20]):
     print(f"Prediction : {pred}  -- Actual : {act}")
     
 
-ConfusionMatrixDisplay.from_predictions(y_test, predictions)
-plt.title("Confusion Matrix")
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+
+ConfusionMatrixDisplay.from_predictions(y_test, predictions, ax=axes[0])
+axes[0].set_title("Confusion Matrix")
+
+bar = axes[1].bar(df_result['param_classifier__n_neighbors'], df_result['mean_test_score'], color="#136D1F", edgecolor="#000000", linewidth=1.2)
+axes[1].set_xlabel("K")
+axes[1].set_ylabel("Accuracy")
+axes[1].set_title("K vs Accuracy")
+axes[1].set_xticks(df_result['param_classifier__n_neighbors'])
+axes[1].grid(True, alpha=0.6)
+axes[1].bar_label(bar, padding=3, fmt='%.4f')
+axes[1].set_axisbelow(True)
 
 plt.tight_layout()
 plt.show()

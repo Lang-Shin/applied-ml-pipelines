17855e3c6aeb366bb1affa9ae2fe90d973dc7e15
Charles <bajuracharles@gmail.com>
2026-07-06 13:42:30 +0800

    KMeans

--- k-means/main.py
+++ k-means/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 
 
 df = pd.read_csv("customer_segmentation_data.csv", usecols=["Annual_Income_k","Spending_Score"])
